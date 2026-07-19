79ff2a908bd9410e08ba99c374d5d52ad6dcf09c
azar <azarfttahy96@gmail.com>
2026-07-19 21:31:28 +0330

    Initial HTML commit

---
+++ tamriny.py
@@ -0,0 +1,5 @@
+name="Anna"
+age=27
+height=1.78
+print("Hi {} I'm {} years old and {}m tall.".format(name,age,height))
+print(f"next year you be {age+1}!")
---
+++ terminal.py
@@ -0,0 +1,24 @@
+print("Hello World")
+#print(4*2)
+#print(float(6/8**3))
+#print(4-2)
+#print(10-2)
+#...........
+#first=int(input(""))
+#second=int(input(""))
+#print(first*second)
+#.................
+#pizza_price=140
+#pizza_num=12
+#doogh_price=400
+#doogh_num=6
+#jaa_price=6000
+#jaa_number=3
+#nafrat=3
+#kole_kharj=(pizza_price*pizza_num)+(doogh_price*doogh_num)+(jaa_price*jaa_number)
+#print(kole_kharj)
+#print(kole_kharj/nafrat)
+#.................
+#message="hello. My name is azar"
+#print(message)
+#print("message")
